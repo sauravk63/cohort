7e3e5b3c9cde5a255253d35d6ce0dde797d5420e
Saurav Kumar <67901218+sauravk63@users.noreply.github.com>
2024-09-18 01:38:21 +0000

    updated

--- week-6/week-6-project/src/App.jsx
+++ week-6/week-6-project/src/App.jsx
@@ -30,50 +30,84 @@ import React, { useState } from 'react'
 //   )
 // })
 
+// let counter = 4;
+// function App(){
+//   const [todos, setTodos] = useState([{
+//     id :1,
+//     title : 'go to gym',
+//     description : 'going to gym'
+//   },
+//   {
+//     id :2,
+//     title : 'eat food',
+//     description : 'eating you food'
+//   },
+//   {
+//     id :3,
+//     title : 'play cricket',
+//     description : 'playing cricket'
+//   }
+// ])
+// function addTodo(){
+
+//   setTodos([...todos, {
+//     id:counter++,
+//     title : Math.random(),
+//     description : Math.random()
+//   }])
+// }
+//   return (
+//     <div>
+//       <button onClick={addTodo}>Add a todo</button>
+//     {todos.map(todo=><Todo title={todo.title} description={todo.description} key={todo.id}/> )}
+//     </div>
+//   )
+// }
+
+// function Todo({title, description}){
+//   return(
+//     <div>
+//       <i>
+//         {title}
+//       </i>
+//       &nbsp; - &nbsp;
+//       <b>
+//         {description}
+//       </b>
+//       <hr />
+//     </div>
+//   )
+// }
+
 function App(){
-  const [todos, setTodos] = useState([{
-    id :1,
-    title : 'go to gym',
-    description : 'going to gym'
-  },
-  {
-    id :2,
-    title : 'eat food',
-    description : 'eating you food'
-  },
-  {
-    id :3,
-    title : 'play cricket',
-    description : 'playing cricket'
-  }
-])
-function addTodo(){
-
-  setTodos([...todos, {
-    id:4,
-    title : Math.random(),
-    description : Math.random()
-  }])
-}
-  return (
-    <div>
-      <button onClick={addTodo}>Add a todo</button>
-    {todos.map(todo=><Todo title={todo.title} description={todo.description} /> )}
-    </div>
-  )
-}
-
-function Todo({title, description}){
   return(
     <div>
-      <h2>
-        {title}
-      </h2>
-      <h3>
-        {description}
-      </h3>
+      <CardWrapper>
+        <TextComponent />
+      </CardWrapper>
+      <br />
+      <CardWrapper>
+        hello there
+      </CardWrapper>
     </div>
   )
 }
+
+function CardWrapper({children}){
+  console.log(children);
+  
+  return (
+    <div style={{border:'2px solid black', padding:20}}>
+      {children}
+    </div>
+  )
+}
+
+function TextComponent(){
+  return <div>
+    hello from text c
+  </div>
+}
+
 
 export default App
