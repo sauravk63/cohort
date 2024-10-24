67b457a1bc8979d46b57467192fe729425b6a595
Saurav Kumar <67901218+sauravk63@users.noreply.github.com>
2024-10-24 18:56:30 +0000

    added all the changes
    all the changes made have been added

--- dummy/practise/file1.js
+++ dummy/practise/file1.js
@@ -1,1 +1,13 @@
-console.log(true == 'true');
+function allNumbers(){
+    console.log('start');
+    printNumbers(4)
+    console.log('end'); 
+}
+
+const printNumbers = (num) => {
+    for (let index = 0; index < num ; index+=2) {
+    console.log(index);    
+    }
+}
+
+allNumbers();
