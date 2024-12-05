8b40d2c2275a39bb3f2182939d70d2027951b9ef
Saurav Kumar <67901218+sauravk63@users.noreply.github.com>
2024-12-05 01:43:28 +0000

    updated projects

--- week-8/week8.1/basic-tailwind-prtc/src/components/Test.jsx
+++ week-8/week8.1/basic-tailwind-prtc/src/components/Test.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Test = () => {
   return (
     <>
-    <div className='grid  grid-cols-1 md:grid-cols-3'>
+    <div className='grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
     <div className='bg-red-500'>
         hello
     </div>
