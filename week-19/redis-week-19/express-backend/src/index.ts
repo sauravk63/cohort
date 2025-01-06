a1e478d0e6d4a6988a27b7afe5c6951484474ea0
Saurav Kumar <67901218+sauravk63@users.noreply.github.com>
2025-01-06 03:08:43 +0000

    week-19 updates

---
+++ week-19/redis-week-19/express-backend/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { createClient } from "redis";
+
+const app = express();
+app.use(express.json());
+
+const client = createClient();
+
+client.connect();
+
+app.post('/submit', async (req, res)=>{
+    const {problemId, userId, code, language} = req.body;
+    try{
+        await client.lPush('submissions', JSON.stringify({problemId, userId, code, language}));   
+        res.json({message : 'Submission received'})
+    }
+    catch(e){
+        res.json({
+            message : 'Failed to submit'
+        })
+    }
+})
+
+app.listen(3000, ()=>{
+    console.log('Connection is open now ');
+    
+});
