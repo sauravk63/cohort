961da3d71ff0f3a22fa3a210ded760979ef33726
Saurav Kumar <67901218+sauravk63@users.noreply.github.com>
2025-01-11 07:15:36 +0000

    finished next project

--- week-14/next-complete-app/app/api/user/route.tsx
+++ week-14/next-complete-app/app/api/user/route.tsx
@@ -1,13 +1,33 @@
 
 import { NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 
 export async function POST(req : NextRequest){
 
     const body = await req.json();
-    console.log(body);
+    // console.log(body);
+
+    const user = await client.user.create({
+        data :{
+            username : body.username,
+            password : body.password
+        }
+    })
+
+    console.log(user.id);
+    
 
     return Response.json({
-        message : "You are now signed up"
+        message : "signed up"
+    })
+}
+
+export function GET(){
+    console.log("User signed up");
+    return Response.json({
+        message : "signed up"
     })
 }
--- week-14/next-complete-app/app/componnets/Signup.tsx
+++ week-14/next-complete-app/app/componnets/Signup.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import { ChangeEventHandler, useState } from "react";
 
 export function Signup() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const router = useRouter(); 
 
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
@@ -29,6 +31,7 @@ export function Signup() {
                                 username, password
                             }
                             ) 
+                            router.push('/');
                         }}
                         className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
                     </div>
