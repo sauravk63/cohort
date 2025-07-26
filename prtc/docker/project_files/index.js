10e84cc3aadd98f2b7ecd0a57ef73aa4ebf58977
Saurav Kumar <67901218+sauravk63@users.noreply.github.com>
2025-07-26 18:29:59 +0000

    published first docker image

--- prtc/docker/project_files/index.js
+++ prtc/docker/project_files/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res)=>{
-    res.send("Hello, node!")
+    res.send("Hello, node!. (sent from docker)")
 })
 
 app.listen(3000,()=>console.log("listening on port 3000"))
