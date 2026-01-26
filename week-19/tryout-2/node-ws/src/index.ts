443edb938fa73c60a0db26a3fbfcabfcb3ff13c0
Saurav Kumar <67901218+sauravk63@users.noreply.github.com>
2026-01-26 07:52:26 +0000

    started with WebSockets

---
+++ week-19/tryout-2/node-ws/src/index.ts
@@ -0,0 +1,30 @@
+import WebSocket, {WebSocketServer} from "ws";
+import http from "http";
+
+const server = http.createServer((req, res)=>{
+    console.log(new Date(), 'Received request for ' + req.url);
+    res.writeHead(200,{ 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+});
+const wss = new WebSocketServer({ server });
+
+let userCount = 0;
+
+wss.on('connection', (ws) => {
+    ws.on('error', console.error);
+    ws.on('message', (data, isBinary) => {
+        wss.clients.forEach((client) => {
+            if(client.readyState === WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
+        }
+        );
+    })
+    console.log("User count: " + ++userCount);
+    ws.send("Message from server 1");
+});
+
+server.listen(8080, () => {
+    console.log(new Date(), 'Server is listening on port 8080');
+});
+
