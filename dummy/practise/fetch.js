56bccb83bcbc7bf55825b1bf3a6c3ca003b54dd8
Saurav Kumar <67901218+sauravk63@users.noreply.github.com>
2026-03-13 19:38:17 +0000

    started with axios

--- dummy/practise/fetch.js
+++ dummy/practise/fetch.js
@@ -1,9 +1,36 @@
-function main(){
-    fetch("https://my-app.nobodyhasitall.workers.dev/")
-    .then(async (resp)=>{
-        const json = await resp.json()
-        console.log(json);
+const axios = require('axios');
+
+
+// function main(){
+//     fetch("https://www.postb.in/1773428751486-2838960215449",{
+//         Method: "POST"
+//     })
+//     .then(async (resp)=>{
+//         const json = await resp.text()
+//         console.log(json);
+//     })
+// }
+
+// async function main(){
+//     const resp = await axios.get("https://my-app.nobodyhasitall.workers.dev/")
+//     const data = resp.data;
+//     console.log(data);
+// }
+
+async function main(){
+    const resp = await axios("https://httpdump.app/dumps/1aacc22b-4987-4fa1-8d0f-9be35aae79f7",
+    {
+        method:"OPTIONS",
+        headers:{
+            Authorization : "Token123$%^"
+        },
+        data:{
+            username:"SAURAV",
+            password:"UGFG&%^GH"
+        }
     })
+    const data = resp.data;
+    console.log(data);
 }
 
 main()
